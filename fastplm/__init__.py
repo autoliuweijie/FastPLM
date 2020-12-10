0bb8bb9022c7a5be609787fd295eaf40621639a5
刘伟杰 <liuweijie@jagerliu-mb1.tencent.com>
2020-12-10 11:11:14 +0800

    init pypi

---
+++ fastplm/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+
+class FastPLM(object):
+
+    def __init__(self,
+                 **kwargs):
+        raise NotImplementedError("FastPLM toolkit will be avalible after approval by Tencent.")
